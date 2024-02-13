baadc96c8252e3d6b64a2426ab3ac3a76ec007ca
Zee-Blazer <ganiyu.bo.bolaji@gmail.com>
2024-02-13 22:46:08 +0100

    Trying something new

--- src/app/message/page.tsx
+++ src/app/message/page.tsx
@@ -2,8 +2,33 @@
 export default function Message() {
 
     return (
-        <div>
-            Something simple
-        </div>
+        <>
+            <div
+                className="bg-cover bg-center mt-12 mx-4 rotate-1 rounded-sm"
+                style={{
+                    backgroundImage: `url("../../../Images/pink.jpg")`,
+                    minHeight: "200px",
+                }}
+            >
+                <div className=" px-5 py-12 h-fit"
+                    style={{ minHeight: "200px", backgroundColor: "rgba(245, 39, 145, 0.59)", color: "black" }}
+                >
+                    <p className="mb-4 caveat text-3xl">Dear Friend,</p>
+
+                    <p className="caveat text-2xl">
+                    Something new
+                    </p>
+
+                    <p className="caveat text-3xl text-right mt-6">
+                        <p>Much Love</p>
+                        Daniel
+                    </p>
+
+                </div>
+
+            </div>
+
+            <p className="text-center py-3 roboto" style={{ color: "lightblue" }}>Send Message</p>
+        </>
     )
 }
